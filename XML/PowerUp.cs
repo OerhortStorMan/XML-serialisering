e8f1dda69327663c41febc08aae195464a00c861
lovewinelid <love.winelid@elev.ga.ntig.se>
2020-09-16 11:05:35 +0200

    Anteck.

---
+++ XML/PowerUp.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace XML
+{
+    public class PowerUp
+    {
+        public int x = 0;
+        public int y = 0;
+        public string name = "";
+
+        
+    }
+}
--- XML/Program.cs
+++ XML/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+    using System.IO;
+    using System.Xml.Serialization;
 
 namespace XML
 {
@@ -6,7 +8,30 @@ namespace XML
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // PowerUp p = new PowerUp();
+
+            // p.name = "fungi";
+            // p.x = 4;
+            // p.y = 3;
+
+            XmlSerializer serializer = new XmlSerializer(typeof(PowerUp));
+
+            FileStream myFile = File.Open("powerup.xml", FileMode.OpenOrCreate);
+
+            // serializer.Serialize(myFile, p);
+
+            PowerUp p = (PowerUp) serializer.Deserialize(myFile);
+
+            myFile.Close();
+
+            System.Console.WriteLine(p.name);
+
+
+            // 3 56 90
+
+            // 3|56|90
+
+            Console.ReadLine();
         }
     }
 }
